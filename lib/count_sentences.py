ee260af07c8d90fb4b4a0b3a310cde1274635d86
Sundeep <sundeep.kaler@gmail.com>
2024-09-10 17:18:53 -0400

    complete lab

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,47 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+  def __init__(self, value = ''):
+    if not isinstance(value, str):
+      print("The value must be a string")
+    else:
+      self.value = value
+  
+  @property
+  def value(self):
+      return self._value
+
+  @value.setter
+  def value(self, value):
+      if not isinstance(value, str):
+        print("The value must be a string.")
+      else:
+        self._value  = value
+
+  def is_sentence(self):
+    if self._value.endswith('.'): 
+      return True
+    else:
+      return False
+  
+  def is_question(self):
+    if self._value.endswith('?'): 
+      return True
+    else:
+      return False
+
+  def is_exclamation(self):
+    if self._value.endswith('!'): 
+      return True
+    else:
+      return False
+
+  def count_sentences(self):
+    sentence_count = 0
+    previous_char = None
+    for character in self._value:
+        if character in '.!?':
+            if previous_char != character:
+                sentence_count += 1
+        previous_char = character
+    return sentence_count
